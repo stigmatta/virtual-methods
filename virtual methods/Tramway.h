cf23f4647f5ee928cd9f1a07cd45d4e36150c5f8
stigmatta <134505496+stigmatta@users.noreply.github.com>
2023-10-04 15:48:20 +0300

    Update Tramway.h

--- virtual methods/Tramway.h
+++ virtual methods/Tramway.h
@@ -10,15 +10,15 @@ class Tramway:public Transport
 public:
 	Tramway();
 	Tramway(string fuel, string model, float max_speed, unsigned int wheels, string tram_number, unsigned int length, unsigned int width, unsigned int passenger_capacity);
-	string getTramNumber();
-	unsigned int getLength();
-	unsigned int getWidth();
-	unsigned int getPassengerCapacity();
+	string getTramNumber()const;
+	unsigned int getLength()const;
+	unsigned int getWidth() const;
+	unsigned int getPassengerCapacity()const;
 	void setTramNumber(string tram_number);
 	void setLength(unsigned int length);
 	void setWidth(unsigned int width);
 	void setPassengerCapacity(unsigned int passenger_capacity);
 	void init();
-	void print();
+	void print()const;
 };
 
